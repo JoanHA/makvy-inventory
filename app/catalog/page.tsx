a7585982ad389974729dc9728ab5cc4fcab95acf
SebastianHurtado19 <joan.hurtado@infodeclat.com>
2024-10-07 22:07:12 -0500

    Pantalla principal con vista web Lista. Pendiente Responsive

---
+++ app/catalog/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <>
+      {/* Buscador */}
+      <div>Buscador</div>
+      {/* Cuerpo */}
+      <div>
+        {/* Categorias - ordenar por .*/}
+        <div>Categorias</div>
+        {/* Productos */}
+        <div>Todos los productos</div>
+      </div>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,83 @@
 import Image from "next/image";
-
+import "@/app/static/CSS/home.css";
+import Link from "next/link";
+import logo from "@/app/static/Img/Logo.jpeg";
+import { ImWhatsapp } from "react-icons/im";
+import { SlOptionsVertical } from "react-icons/sl";
+import { ICON_WIDTH } from "./constants/constants";
+import { LiaStoreAltSolid } from "react-icons/lia";
+import { FaInstagram, FaTiktok, FaWhatsapp, FaStore } from "react-icons/fa6";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 ">
+      <main className="flex flex-col gap-8 row-start-2 items-center  h-100  w-[50%] max-md:w-[100%]">
+        <div
+          id="title-container"
+          className="flex justify-center content-center  flex-wrap  flex-col items-cente  "
+        >
+          <div className="rounded">
             <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+              src={logo}
+              alt="Page logo"
+              height={100}
+              className="object-cover rounded-full "
+            ></Image>
+          </div>
+          <div className="flex flex-col items-center main-title-container">
+            <h2 className="main-title">Makvy_acccesorios</h2>
+            <p className="text-center">Tu tienda favorita</p>
+          </div>
+        </div>
+        <div className="w-full ">
+          <div
+            id="options-container"
+            className="grid  md:grid-cols-1 sm:w-100 sm:h-90 grid-cols-2 gap-6 w-full p-0 h-full"
           >
-            Read our docs
-          </a>
+            <div className="+ link-container max-md:h-[170px] ">
+              <Link
+                href={"/catalog"}
+                className="home-options w-full h-full text-center flex  md:h-[70px] "
+              >
+                <FaStore size={ICON_WIDTH} />
+                Catálogo
+                <SlOptionsVertical />
+              </Link>
+            </div>
+            <div className="link-container max-md:h-[170px]   ">
+              <Link
+                href={"/catalog"}
+                className="home-options w-full h-full text-center md:h-[70px]"
+              >
+                <FaWhatsapp size={ICON_WIDTH} />
+                Whatsapp
+                <SlOptionsVertical />
+              </Link>
+            </div>
+            <div className="link-container max-md:h-[170px] ">
+              {" "}
+              <Link
+                href={"/catalog"}
+                className="home-options w-full h-full text-center md:h-[70px]"
+              >
+                <FaInstagram size={ICON_WIDTH} />
+                Instagram
+                <SlOptionsVertical />
+              </Link>
+            </div>
+            <div className="link-container max-md:h-[170px] ">
+              {" "}
+              <Link
+                href={"/catalog"}
+                className="home-options w-full h-full text-center md:h-[70px]" 
+              >
+                <FaTiktok size={ICON_WIDTH} />
+                Tik tok
+                <SlOptionsVertical />
+              </Link>
+            </div>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
